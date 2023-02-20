97e0ed6ce5f2b7100bfe9e9c796a7569df8e497d
Aditya S. Camarushy <aditcam@Adityas-MacBook-Air.local>
2023-02-20 17:33:52 -0500

    Project restructuring and addition of project and app components

--- weatherProject/urls.py
+++ weatherProject/urls.py
@@ -1,4 +1,4 @@
-"""weatherApp URL Configuration
+"""weatherProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
