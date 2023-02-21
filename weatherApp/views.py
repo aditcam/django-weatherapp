05f83b4f9737259b923268d3dafeba074e7dc271
Aditya S. Camarushy <aditcam@Adityas-MacBook-Air.local>
2023-02-20 19:36:48 -0500

    Webapp completed

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+import urllib.request
+import json
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=af29c6c8a0bcfd5dae496a9c4922810e'.format(city)).read()
+        list_of_data = json.loads(source)
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon'])+', '+str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp'])+ '°C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']), 
+            "main": str(list_of_data['weather'][0]['main']), 
+            "description": str(list_of_data['weather'][0]['description']), 
+            "icon": list_of_data['weather'][0]['icon'], 
+        }
+        print(data)
+    else:
+        data = {}
+    
+    return render(request, "main/index.html", data)
